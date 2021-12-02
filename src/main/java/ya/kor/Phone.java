dfbb027058747cc434398b3a330ace1fc58a4d9c
yana <veryutina.yana@yandex.ru>
2021-12-02 18:05:10 +0300

    add homework

--- src/main/java/ya/kor/Phone.java
+++ src/main/java/ya/kor/Phone.java
@@ -2,6 +2,36 @@ package ya.kor;
 
 public class Phone {
 
-        int weight;
-        String model;
+    int weight;
+    String model;
+
+    public void sayWeight() {
+        System.out.println(weight);
+    }
+
+    public void sayModel() {
+        System.out.println(model);
+    }
+        public void incrementWeight() {
+                System.out.println(weight);
+                weight = weight + 100;
+        }
+
+        public static void main(String[] args) {
+            Phone phone = new Phone();
+            phone.model = "iPhone XR";
+            phone.weight = 204;
+            Phone samsung = new Phone();
+            samsung.model = "Samsung S7";
+            samsung.weight = 150;
+            phone.sayModel();
+            samsung.sayModel();
+
+            phone.incrementWeight();
+            phone.sayWeight();
+            samsung.sayWeight();
+
+        }
 }
+
+
