ae027b7b17edb5c66753faf717db840f3d80c8e7
YanaVeryutina <veryutina.yana@yandex.ru>
2022-01-17 00:34:59 +0300

    Addes new classes

---
+++ src/main/java/ya/kor/Cat.java
@@ -0,0 +1,16 @@
+package ya.kor;
+
+public class Cat {
+    String name;
+    String color;
+    int weight;
+    int age;
+
+    void sayname() {
+        System.out.println("Cats name is" + name);
+    }
+    void toy(Toy toy){
+
+    }
+
+}
---
+++ src/main/java/ya/kor/Toy.java
@@ -0,0 +1,17 @@
+package ya.kor;
+
+public class Toy {
+    String typeOfToy;
+
+
+    public Toy(String typeOfToy) {
+        System.out.println("Любимая игрушка");
+    }
+
+    @Override
+    public void typeOfToy() {
+        System.out.println();
+    }
+
+
+}
